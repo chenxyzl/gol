e837ea6fa79d1a870cf9a5909392fec65e52b2f6
zhonglin.chen <zhonglin.chen@funplus.com>
2022-03-28 14:12:23 +0800

    init

---
+++ tools/pre-commit.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+git status -uno| grep -e modified -e "new file"  > check.txt
+cmd="du -s"
+# cat check.txt|while read line;
+while read line
+do
+line=${line#*:}
+# ls -lht $line
+cmd="$cmd $line"
+done<check.txt
+cmd="$cmd | sort -hr"
+eval $cmd > check.txt
+while read line
+do
+size=`echo $line | awk -F" " '{print $1}'`
+name=`echo $line | awk -F" " '{print $2}'`
+if [ $size -gt 10485760 ]; then
+echo "file:$name too big"
+exit 1
+fi
+# line=${line%%"\t"*}
+# line=eavl "echo $line | cut -d$'\t' -f2"
+done<check.txt
