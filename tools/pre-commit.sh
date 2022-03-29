6ea0bd7d02f17c627b44bcef5c1bc1f465ffcd1d
zhonglin.chen <zhonglin.chen@funplus.com>
2022-03-29 18:31:01 +0800

    1

--- tools/pre-commit.sh
+++ tools/pre-commit.sh
@@ -1,13 +1,26 @@
 #!/bin/sh
+
+# get files
 git status -uno| grep -e modified -e "new file"  > check.txt
-cmd="du -sk"
+# must not use ｜
 # cat check.txt|while read line;
+
+#get add/modify files sorted by disk use size
+cmd="du -sk"
+changed=0
 while read line
 do
+changed=1
 line=${line#*:}
-# ls -lht $line
 cmd="$cmd $line"
 done<check.txt
+
+#no file change exit im
+if [ $changed -eq 0 ]; then
+exit 0
+fi
+
+# check size
 cmd="$cmd | sort -hr"
 eval $cmd > check.txt
 while read line
@@ -18,6 +31,4 @@ if [ $size -gt 10240 ]; then
 echo "file:$name too big"
 exit 1
 fi
-# line=${line%%"\t"*}
-# line=eavl "echo $line | cut -d$'\t' -f2"
 done<check.txt
